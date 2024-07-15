838578173815742f54f860e9444d0e00c99e0109
Cecepagus06 <cecepagus406@gmail.com>
2024-07-15 18:33:29 +0700

    15-7-2024

--- index.java
+++ index.java
@@ -1,6 +1,11 @@
 public class  index{
   public static void main(String[] args){
     System.out.println("hello");
+    String nama;
+    nama = "cecep agus";
+    int umur;
+    umur = 16;
+    System.out.println("hallo nama saya "+nama+", umur saya "+umur);
   }
   
 }
