16b77b611fec55e862ad2ec6f210f77b52fe08a3
Cecepagus06 <cecepagus406@gmail.com>
2024-07-20 15:59:56 +0700

    20-7-2024

---
+++ kalkulator.java
@@ -0,0 +1,19 @@
+
+import java.util.Scanner;
+
+public class kalkulator{
+  public static void main(String[] args){
+    Scanner input = new Scanner(System.in);
+   System.out.print("masukkan angka: ");
+   double angka = input.nextDouble();
+   
+   if( angka % 2 == 0){
+   System.out.println("angka " +angka+ " adalah angka genap ");
+     
+   }else{
+     
+   System.out.println("angka " +angka+ " adalah angka ganjil ");
+   }
+  }
+  
+}
